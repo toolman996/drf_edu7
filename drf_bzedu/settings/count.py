9c8401fe576e8187288a4d01f8fe250f5be24633
wei <1938785959@qq.com>
2020-07-21 00:47:41 +0800

    支付宝支付

---
+++ drf_bzedu/settings/count.py
@@ -0,0 +1,31 @@
+BANNER_COUNT=3
+NAVBAR_COUNT=5
+# 验证码发送间隔时间
+SMS_EXPIRE_TIME = 60
+# 验证码的有效时间
+MOBILE_EXPIRE_TIME = 10 * 60
+
+# 单条发送的接口
+SINGLE_SEND_URL = "https://sms.yunpian.com/v2/sms/single_send.json"
+# 发送短信的APIKEY
+API_KEY = "ee6be70a1c7f33e884e16ab9e6ef91f6"
+
+# 图片路径
+IMAGE_URL="http://code.toolman.org:9001"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
